a6ecc08043d864695c9ce82d594fb64c93ac4b1b
Selasi <kudolos@gmail.com>
2020-04-12 20:15:33 -0400

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Selasi Kudolo on 2020-04-12.
+//  Copyright © 2020 Ewe Cat Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
